740f948c3375a134f7c51eeb251207ce0ed6ff1a
zhangxq2019 <zhangxq0306@foxmail.com>
2021-01-27 16:49:37 +0800

    填写 icon 和 iconPositon 属性

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,31 @@
 <template>
-  <button class="g-button">按钮</button>
+  <div>
+<!--    <button class="g-button" v-if="iconPosition === 'right'">-->
+<!--      <svg v-if="icon" class="icon">-->
+<!--        <use :xlink:href="`#i-${icon}`"></use>-->
+<!--      </svg>-->
+<!--      <slot></slot>-->
+<!--    </button>-->
+<!--    <button class="g-button" v-else>-->
+<!--      <slot></slot>-->
+<!--      <svg v-if="icon" class="icon">-->
+<!--        <use :xlink:href="`#i-${icon}`"></use>-->
+<!--      </svg>-->
+<!--    </button>-->
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+      <svg v-if="icon" class="icon">
+        <use :xlink:href=`#i-${icon}`></use>
+      </svg>
+     <span class="content">
+        <slot></slot>
+     </span>
+    </button>
+  </div>
 </template>
 <script>
-export default{}
+export default {
+  props: ['icon','iconPosition']
+}
 </script>
 <style lang="scss">
 .g-button {
@@ -13,14 +36,36 @@ export default{}
   background: var(--button-bg);
   border: 1px solid var(--border-color);
   border-radius: var(--border-radius);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
   &:hover {
-  border-color: var(--border-color-hover);
+    border-color: var(--border-color-hover);
   }
-  &:active{
-  background-color: var(--button-active-bg);
+
+  &:active {
+    background-color: var(--button-active-bg);
   }
+
   &:focus {
-  outline: none;
+    outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: .3em;
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+     > .icon {
+       order: 2;
+       margin-right: 0;
+       margin-left: .3em;
+     }
+    > .content {
+      order: 1;
+    }
   }
 }
 </style>
