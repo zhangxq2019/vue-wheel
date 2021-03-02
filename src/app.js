a6a306d9cee866c30e1dda3b948c2fb2baafdc73
zhangxq2019 <zhangxq0306@foxmail.com>
2021-03-02 12:51:46 +0800

    添加input vue 及其样式

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import Input from './input'
+
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGroup)
+Vue.component('g-input',Input)
 new Vue({
     el: '#app',
     data: {
@@ -14,88 +17,3 @@ new Vue({
     }
 })
 
-//单元测试
-//四个部分
-//icon
-//loading
-//iconPosition
-
-import chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
-const expect = chai.expect
-
-{
-    //判断icon是否正确
-    // console.log(Button)
-    const Constructor = Vue.extend(Button)
-    //Vue.extend+vm.$mount组合
-    //Button是一个对象，vue.extend()将Button生成一个构造函数
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting'
-        }
-    })//vm就是Button的实例，可以挂载到任一指定页面
-    vm.$mount()
-    // 动态生成按钮
-    let useElement = vm.$el.querySelector('use')
-    // console.log(useElement)
-    let href = useElement.getAttribute('xlink:href')
-    // console.log(href);
-    expect(href).to.eq('#i-setting')
-}
-
-{
-    //判断loading是否正确
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting',
-            loading: true
-        }
-    })
-    vm.$mount()
-    let useElement = vm.$el.querySelector('use')
-    // console.log(useElement)
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-loading')
-}
-
-{
-    //判断iconPosition的位置 order
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting',
-            iconPosition:'right'
-        }
-    })
-    vm.$mount(div)
-    let svg = vm.$el.querySelector('svg')
-    // console.log(svg);
-    let {order} = window.getComputedStyle(svg)
-    // console.log(order)
-    expect(order).to.eq('2')
-    vm.$el.remove()
-    vm.$destroy()
-}
-{
-    //
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting',
-        }
-    })
-    vm.$mount('.test')
-    //生成一个间谍函数代替原来的函数，如果click触发了就执行这个间谍函数
-    let spy = chai.spy(()=>{})
-    vm.$on('click',spy)
-    let button = vm.$el
-    //点击
-    button.click()
-    //判断间谍函数是否被调用了
-    expect(spy).to.have.been.called()
-}
---
+++ src/input.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="wrapper" :class="{error}">
+    <input :value='value' type="text" :readonly="readOnly" :disabled="disabled"/>
+    <template v-if="error">
+      <icon name="error"></icon>
+      <span class="errorMessage">{{ error }}</span>
+    </template>
+  </div>
+</template>
+<script>
+import Icon from './icon'
+
+export default {
+  components: {Icon},
+  props: {
+    value: {
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readOnly: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String,
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+$height: 32px;
+$border-color: #999;
+$box-shadow-color: #333;
+$border-color-hover: red;
+.wrapper {
+  font-size: 12px;
+  display: inline-flex;
+  align-items: center;
+  //border: 1px solid red;
+
+  > input {
+    height: 32px;
+    border: 1px solid var $border-color;
+    border-radius: 4px;
+    padding: 0 8px;
+    font-size: inherit;
+
+    &:hover {
+      border-color: $border-color-hover;
+    }
+
+    &:focus {
+      box-shadow: inset 0 1px 3px $box-shadow-color;
+      outline: none;
+    }
+
+    &[disabled], &[readonly] {
+      border-color: #bbb;
+      color: #bbb;
+      cursor: not-allowed;
+    }
+
+    .icon-error {
+      fill: red;
+    }
+
+  }
+
+  &.error {
+    > input {
+      border-color: yellow;
+    }
+  }
+
+  .errorMessage {
+    color: red;
+  }
+}
+</style>
