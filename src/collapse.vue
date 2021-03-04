bd0a8809ccacb52e9b9d38ba0e8edede30cd3329
zhangxq2019 <zhangxq0306@foxmail.com>
2021-03-04 19:37:54 +0800

    可以设置默认selected项

--- src/collapse.vue
+++ src/collapse.vue
@@ -4,8 +4,32 @@
   </div>
 </template>
 <script>
+import Vue from 'vue'
+
 export default {
-  name: 'GCollapse'
+  name: 'GCollapse',
+  props: {
+    single: {
+      type: Boolean,
+      default: false
+    },
+    selected: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      eventBus: new Vue()
+    }
+  },
+  provide() {
+      return {
+        eventBus: this.eventBus
+    }
+  },
+  mounted(){
+    this.eventBus.$emit('update:selected',this.selected)
+  }
 }
 </script>
 <style lang="scss" scoped>
